36e6f93e4844f473b865bf041b238fadd2c50025
Liana <95958723+Lianatim@users.noreply.github.com>
2022-11-20 14:26:40 +0300

    7. HTML form. Создание вакансии.

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -1,18 +1,24 @@
 package ru.job4j.dreamjob.model;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Post {
     private int id;
     private String name;
+    private String desc;
+    private LocalDateTime created;
 
     public Post() {
     }
 
-    public Post(int id, String name) {
+    public Post(int id, String name, String desc, LocalDateTime created) {
         this.id = id;
         this.name = name;
+        this.desc = desc;
+        this.created = created;
     }
+
 
     public int getId() {
         return id;
@@ -28,6 +34,22 @@ public class Post {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,9 +14,9 @@ public class PostStore {
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
+        posts.put(1, new Post(1, "Junior Java Job", "Наша команда разрабатывает программное решение для финансового сектора.", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "Ищем Android-программиста в молодую развивающуюся команду.", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "Мы IT-компания из Санкт-Петербурга, которая занимается разработкой мобильных приложений, веб-порталов.", LocalDateTime.now()));
     }
 
     public static PostStore instOf() {
