0b1cd6aace9725eeab4dc666e72e01b4b98b0d00
Liana <95958723+Lianatim@users.noreply.github.com>
2022-12-04 10:59:16 +0300

    3. Filter

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -1,10 +1,13 @@
 package ru.job4j.dreamjob.filter;
 
 import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 @Component
 public class AuthFilter implements Filter {
@@ -17,7 +20,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (success(uri)) {
             chain.doFilter(req, res);
             return;
         }
@@ -26,5 +29,28 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    enum UserRegistered {
+        LOGIN_PAGE("loginPage"),
+        LOGIN("login"),
+        FORM_ADD_USER("formAddUser"),
+        FAIL("fail"),
+        SUCCESS("success"),
+        REGISTRATION("registration");
+
+        private final String name;
+
+        UserRegistered(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private boolean success(String uri) {
+        return Arrays.stream(UserRegistered.values()).anyMatch(value -> uri.endsWith(value.getName()));
     }
 }
